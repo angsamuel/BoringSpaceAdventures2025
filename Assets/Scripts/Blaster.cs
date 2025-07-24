f9036200f4c25e7d1a8a1470432ab48199f64b55
Samuel Ang <samuel.ang.prog@gmail.com>
2025-07-24 15:58:36 -0500

    fixed a few minor issues

--- Assets/Scripts/Blaster.cs
+++ Assets/Scripts/Blaster.cs
@@ -229,7 +229,7 @@ public class Blaster : Item
         base.Update();
         Kickback();
         DecaySpread();
-        Aiming();
+        DownSightsUpdate();
     }
 
     public override void Reload(){
